035ba83ba82168402e0814169b4fb965a3975809
Fares Mohamed <fm3607477@gmail.com>
2025-08-28 12:30:25 +0300

    updated JS file

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// setTimeout(() => {
-//   console.log("Download photo from url");
-//   setTimeout(() => {
-//     console.log("Resize photo");
-//     setTimeout(() => {
-//       console.log("Add Logo to photo");
-//       setTimeout(() => {
-//         console.log("show the photo in website");
-//       }, 4000);
-//     }, 3000);
-//   }, 2000);
-// }, 1000);
+setTimeout(() => {
+  console.log("Download photo from url");
+  setTimeout(() => {
+    console.log("Resize photo");
+    setTimeout(() => {
+      console.log("Add Logo to photo");
+      setTimeout(() => {
+        console.log("show the photo in website");
+      }, 4000);
+    }, 3000);
+  }, 2000);
+}, 1000);
 
 function delay(ms, message) {
   return new Promise((resolve) => {
@@ -21,13 +21,13 @@ function delay(ms, message) {
 }
 
 // Chaining Promises
-// delay(1000, "Download photo from url")
-//   .then(() => delay(2000, "Resize photo"))
-//   .then(() => delay(3000, "Add Logo to photo"))
-//   .then(() => delay(4000, "Show the photo in website"))
-//   .catch((err) => {
-//     console.error("Something went wrong:", err);
-//   });
+delay(1000, "Download photo from url")
+  .then(() => delay(2000, "Resize photo"))
+  .then(() => delay(3000, "Add Logo to photo"))
+  .then(() => delay(4000, "Show the photo in website"))
+  .catch((err) => {
+    console.error("Something went wrong:", err);
+  });
 
 function deelay(ms, message) {
   return new Promise((res, rej) => {
